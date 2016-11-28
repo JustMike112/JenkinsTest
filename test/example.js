75fbcb965224d22e2c92998150e4f95857588a9b
Mike Schouten <justmike112@hotmail.com>
2016-11-28 15:01:15 +0100

    First Test
    Just a basic test that does not even test anything

---
+++ test/example.js
@@ -0,0 +1,32 @@
+// load Unit.js module
+var test = require('unit.js');
+
+//var file = require('../JenkinsTest/public_html/js/tryout.js');
+
+var example = 'Hello world!';
+
+// describes a successfull test as
+// [name]
+// V [description]
+describe('Hello', function() {
+  it('Should return true if Hello world! starts with Hello', function() {
+    test.string(example).startsWith('Hello');
+  });
+});
+
+describe('world', function() {
+  it('Should return true if '+ file + ' contains world', function() {
+    test.string(example).contains('world');
+  });
+});
+describe('...!', function() {
+  it('Should return true if Hello world! contains !', function() {
+    test.string(example).contains('!');
+  });
+});
+
+// describes a successfull test as
+// V [nameTest]
+it('other test case', function(){
+    // empty test case succeeds
+  });
