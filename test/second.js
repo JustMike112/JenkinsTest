b46fef84f1e0f4744d0d5d53afa52c559359fb61
Mike Schouten <justmike112@hotmail.com>
2016-11-29 10:09:40 +0100

    Added a new test
    Still testing jenkins, now with an extra test

---
+++ test/second.js
@@ -0,0 +1,9 @@
+it('Second test runs as well', function(){
+    // empty test case succeeds
+  });
+
+describe('Second Test', function() {
+  it('Just great !', function() {
+    // empty test case succeeds
+  });
+});
